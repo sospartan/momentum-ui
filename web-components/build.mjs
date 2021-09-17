633471c50156768b1b262e0313fb60508ce561c4
sospartan zheng <sospartan@apache.org>
2021-09-17 15:04:25 +0000

    [fix] fix token script: use relative path

--- web-components/build.mjs
+++ web-components/build.mjs
@@ -3,11 +3,13 @@ import { sassPlugin } from "esbuild-sass-plugin";
 
 esbuild.build({
   tsconfig: "./src/tsconfig.json",
-  entryPoints: ["./src/components/table-advanced/TableAdvanced.ts"],
+  entryPoints: ["./src/index.ts"],
   bundle: true,
   logLevel: 'error',
   outdir: "dist/components",
-  plugins: [sassPlugin()],
+  plugins: [sassPlugin({
+    type: "lit-css"
+  })],
   loader: {
     ".woff2": "file",
     ".woff": "file",
